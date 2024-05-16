be1c3e976aacf2fcdd3db86b0ccb0c7583e7ef0f
Shami THIRION SEN <shami.contact@gmail.com>
2024-05-16 23:05:31 +0200

    add

---
+++ scripts/scrape.sh
@@ -0,0 +1,20 @@
+# https://github.com/hartator/wayback-machine-downloader/blob/master/README.md
+
+
+# scrape bengali communist bengali news websites
+
+# wayback_machine_downloader https://www.ganashakti.com -d corpus/ganashakti/ --all-timestamps 
+# wayback_machine_downloader https://www.ganadabi.com -d corpus/ganadabi/ --all-timestamps
+# wayback_machine_downloader https://www.marxbadipath.org -d corpus/marxbadipath/ --all-timestamps
+
+
+# non communist bengali news websites
+# get <p> tags for anandabazar 
+# wayback_machine_downloader https://www.anandabazar.com -d corpus/anandabazar/ --all-timestamps
+# wayback_machine_downloader https://www.bartamanpatrika.com -d ../corpus/bartamanpatrika/ --all-timestamps
+wayback_machine_downloader https://www.eisamay.com -d ../corpus/eisamay/ --all-timestamps
+wayback_machine_downloader https://www.sangbadpratidin.in -d ../corpus/sangbadpratidin/ --all-timestamps
+wayback_machine_downloader https://www.uttarbangasambad.com -d ../corpus/uttarbangasambad/ --all-timestamps
+
+
+
